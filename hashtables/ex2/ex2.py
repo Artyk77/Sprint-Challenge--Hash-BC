6a7f3f5dc3ecd05aaa1a8839054150887dd08d01
jose restrepo <arturo.restrepo@gmail.com>
2020-01-17 13:56:47 -0500

    ex1 and ex2 test passed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,22 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # loop through all the tickets
+    for i in range(0, len(tickets)):
+        
+      # set the first ticket's destination as the first leg in the route
+        if tickets[i].source == "NONE":
+            route[0] = tickets[i].destination
+      # otherwise, insert the legs in the hash-table for retrieval later
+        else:
+            hash_table_insert(
+                hashtable, tickets[i].source, tickets[i].destination)
+
+
+    # loop through the tickets again, finding all the legs whose source is the previous destination
+    # save that leg's destination as the next leg in the route
+    for j in range(1, len(route)):
+        stop = hash_table_retrieve(hashtable, route[j - 1])
+        route[j] = stop
 
     return route
